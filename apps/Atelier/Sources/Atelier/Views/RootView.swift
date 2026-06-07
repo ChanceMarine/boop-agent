96f041a0216e41bb92beec912be6ddd7ca776824
Chance <mail@chancemarine.com>
2026-06-07 18:02:11 -0400

    fix: inset Atelier rail

--- apps/Atelier/Sources/Atelier/Views/RootView.swift
+++ apps/Atelier/Sources/Atelier/Views/RootView.swift
@@ -10,7 +10,8 @@ struct RootView: View {
 
             HStack(spacing: 0) {
                 MainRailView(selection: $store.selectedRoute)
-                    .frame(width: 78)
+                    .frame(width: 72)
+                    .padding(4)
 
                 SubtabSidebarView(mainTab: store.selectedRoute.mainTab, selection: $store.selectedRoute)
                     .frame(width: 240)
